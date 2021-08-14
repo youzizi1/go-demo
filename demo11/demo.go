6b591a08e93b2f9833957df84391a540a7943f72
youzizi1 <yinyun957@gmail.com>
2021-08-15 00:41:25 +0800

    recursion

--- demo11/demo.go
+++ demo11/demo.go
@@ -22,10 +22,19 @@ func fn4(nums ...int) int {
 	return total
 }
 
+// 递归调用
+func fn5(num int) int {
+	if num == 1 {
+		return num		
+	}
+	return fn5(num - 1) + num
+}
+
 func main() {
 	fmt.Println(fn1(1, 2))
 	fmt.Println(fn2(1, 2, 3))
 	fmt.Println(fn3(1, 2))
 	fmt.Println(fn4(1, 2, 3, 4, 5))
 	fmt.Println(fn4(1, 2, 3))
+	fmt.Println(fn5(10))
 }
