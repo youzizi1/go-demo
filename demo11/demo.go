b00c14e621b520032cbc5ea804e53e50f27f2429
youzizi1 <yinyun957@gmail.com>
2021-08-15 00:38:12 +0800

    init demo11

---
+++ demo11/demo.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func fn1(a int, b int) int  {
+	return a + b
+}
+
+func fn2(a, b, c int) int {
+	return a + b + c
+}
+
+func fn3(a, b int) (int, int, int, int)  {
+	return a + b, a - b, a * b, a / b
+}
+
+func fn4(nums ...int) int {
+	total := 0
+	for _, v := range nums {
+		total += v
+	}
+	return total
+}
+
+func main() {
+	fmt.Println(fn1(1, 2))
+	fmt.Println(fn2(1, 2, 3))
+	fmt.Println(fn3(1, 2))
+	fmt.Println(fn4(1, 2, 3, 4, 5))
+	fmt.Println(fn4(1, 2, 3))
+}
