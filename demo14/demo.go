6ea28f5163d2519e1a4321a13b30bda83861e4ec
youzizi1 <yinyun957@gmail.com>
2021-08-15 11:49:53 +0800

    init demo14

---
+++ demo14/demo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type rect struct {
+	width, height int
+}
+
+func (r *rect) area() int  {
+	return r.width * r.height
+}
+
+func (r rect) perim() int  {
+	return 2 * (r.width + r.height)
+}
+
+func main() {
+	r1 := rect{width: 10, height: 20}
+	fmt.Println(r1.area(), r1.perim())
+}
